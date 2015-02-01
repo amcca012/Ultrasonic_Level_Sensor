3b5c74a6a7057ad2c21a5059a4e544798354c1cc
Anthony B McCann <abmcc123@verizon.net>
2015-01-31 20:49:27 -0500

    median filter, temp comp tweak
    Used slightly more accurate speed sound, eliminated some messages. Per
    mfr used median filter. changed modem response testing

--- Old Code/sketch_dec13b/sketch_dec13b.ino
+++ Old Code/sketch_dec13b/sketch_dec13b.ino
@@ -3,7 +3,7 @@ char MESSAGE [160] = "" ;
 //char smsbuffer[160];
 int Zero = 10000; //CM to pavementwill restatrt to level=0
 const int timeDelaySeconds = 600;//Set time between readings
-const int powerpin = 22;//cycle for software start gprs
+const int powerpin = 31;//cycle for software start gprs
 const int txpin = 18;//pins to comm with gprs shield
 const int rxpin = 19;
 const int pwPin = A2;//pulsewidth ranging pin
@@ -20,20 +20,29 @@ int Ilevel = 0 ;//level as level approachs detector from  Izero/ causes level to
 int IVsound;
 int Ifahrenheit = 100;
 //RTC_DS1307 RTC; // define the Real Time Clock object
-char phone1[] = "+17573248688"; //AT command cmgs requires quotation marks around text string
+char phone1[] = "+17577598935";
+//char phone1[] = "+17573248688"; //AT command cmgs requires quotation marks around text string
 char phone2[] = "+17579151048";
-char timeStamp[21] = ("15/01/20,13:15:10+04")
-                     void setup()//*****************************************
+char timeStamp[21] = ("15/01/20,13:15:10+04");
+void setup()//*****************************************
 {
   Serial1.begin(9600);
   Serial.begin(9600);  //Serial1.begin(9600);//Mega shield extra UARTs, ensure correct board is selected
   pinMode(pingPermit, OUTPUT);//d22 range  on HIGH>22us
+  digitalWrite(pingPermit, HIGH); //May just leave on for better readings
   pinMode(powerpin, OUTPUT);//soft ware on/off gprs
   pinMode(txpin, OUTPUT);//HWserial1
   pinMode(rxpin, INPUT);//HWSerial1
-  pinMode(pwPin, INPUT); //Measur range using pw method
+  pinMode(pwPin, INPUT); //Measure range using pw method
+  pinMode(analogIn, INPUT);
   powerup();
   smsMode();
+  Serial1.println("ATE0");
+  int centigrade = analogRead(temperatureIn) * 0.488;
+  Ifahrenheit = (centigrade) * 9 / 4 + 32;
+  IVsound = 331.3 + (centigrade * .6);
+  Ilevel= analogRead(analogIn) * (331.3 / IVsound);
+  Izero=Ilevel;
 }
 
 void loop()//*************************************
@@ -43,18 +52,38 @@ void loop()//*************************************
   takeTime();
   logdata();
   SendTextMessage();
-  delay(timeDelaySeconds * 10000);//This gives 1 minute between readings
+  delay(timeDelaySeconds * 1000);//Will need tweak
 }
 
-takeTime()//should remove+CCLK: then get date and time before LF
+void takeTime()//should remove+CCLK: then get date and time before LF
 {
-  Serial1.print("AT+CCLK");
-  while (!Serial1.available)
+  Serial1.println("AT+");
+  Serial.println("AT+CCLK?");
+  char empty[65] = "";
+  delay(100);
+  Serial1.flush();
+   if (Serial1.available())
   {
+  for(int x=0; x = Serial1.available();  x++)
+  {
+    empty[x] = Serial1.read();
+  }   
   }
-Serial1.readBytesUntil(: , timeStamp);
-  Serial1.readBytesUntil(lf, timeStamp);
-  Serial.println(timeStamp);
+  else
+  {
+    Serial.println("Nothing in buffer");
+  }
+  Serial.println(empty);
+ // while (!Serial1.read() == '/r')
+  //{
+  //  Serial.print(" waiting resp cclk");
+  //  delay(50);
+  //}
+ // Serial.println(Serial1.peek());
+  //Serial1.readBytesUntil('/f' , timeStamp, 20); //:=58
+  //Serial.println(timeStamp);
+  //Serial1.readBytesUntil(10, timeStamp, 19); //lf=10
+  //Serial.println(timeStamp);
 }
 
 void takeTemp()
@@ -68,12 +97,27 @@ void takeTemp()
   Serial.println( " c");
 }
 
-void powerup()// two tries at powering up
+void powerup()// 
 {
+  Serial.println("powerup");
   int x = 0;
-  for (int t = 0; t == 1; t++)//exit on t=2
+  int t = 0;
+  char empty[65] = "";
+  empty[65] = (Serial1.read());
+  Serial.print("powerup -stuff found in serial buffer=> ");
+  //Serial.println(empty);
+  Serial1.println("AT+"); // shoot in test signal, let Serial1Response wait for signal
+  //Serial.println("AT+");//troubleshooting
+  delay(1000);
+  if (Serial1.available())
   {
-    Serial1.println("AT+"); // shoot in test signal, let Serial1Response wait for signal
+  for(x=0; x = Serial1.available();  x++)
+  {
+    empty[x] = Serial1.read();
+  }   
+  }
+  else //actions if nothing available(modem off)
+  {
     Serial1.flush();//Makes sure command is in before try reading
     x = Serial1Response(2); //if x=0 zero, test if alive for 2 sec, returns number of bytes available
     if (x == 0) //something should come back, if it doesnt, power up
@@ -86,25 +130,29 @@ void powerup()// two tries at powering up
       delay(3000);
     }
   }
+     Serial.println(empty);//show what was in serial1, should be response from at+
 }
 
 int Serial1Response(int maxsec )//waits "maxsec" sec maximum for serial1 (default 0 ), returns bytes available
 {
+  Serial.println("checking serial bytes available");
   long int x = 0;
-  for ( x = 0; x == (100 * maxsec); x++)//100*delay of 10 usec*maxsec
+  for ( x = 0; x == (1000 * maxsec); x++)//100*delay of 10 usec*maxsec
   {
     delay(10);
   }
+  Serial.print(Serial1.available());
+  Serial.println(" bytes avaiable from serial 1");
   return (Serial1.available());//return some number bytes available
 }
 void logdata()//               ...........................
 {
-  Serial.print("logging data");
+  Serial.println("logging data");
   // DateTime now = RTC.now();
 
   PString str( MESSAGE, 160  );
   str.print( ",");
-  str.print(timestamp);
+  str.print(timeStamp);
   str.print( ",");
   str.print(PWrange);
   str.print( ",");
@@ -121,52 +169,62 @@ void logdata()//               ...........................
   str.print("Phone2=");
   str.print(phone2);
   str.print(",");
-  Serial.println(str);
+  //Serial.println(str);
   Serial.print("MESSAGE=");
   Serial.println(MESSAGE);
 
 }
 float takeReading()//stores range in global variables
 {
-  analogRead(analogRead(analogIn) / (2 * .49)) * (331.33 / IVsound);
-  digitalWrite(pingPermit, HIGH); //Hold high to range
-  delay(25); //hold >20 us to range
-  int x = 0;
-  int count = 0;
-  for (x = 1; x == 10; x++) //simple traffic or noise filter, avg out spikes,and blocks rapid changes
-    //Think about how much change could occur between readings
+  int I[4];
+  int PW[4];
+  for (int x=0; x==2; x++)
   {
-    while ( PWrange + 50 <= (pulseIn(pwPin, HIGH)) || (Irange + 50 < (analogRead(analogIn)))) //58us/cm,
-      //a car or person should give more than 50 CM increase since last reading unless huge flood
-      //then car may not show 50 cm..., this sonar mod will sense nearest object
-      // a small number not used since a real level may have ocurred if idle very long
-    {
-      count = count++;
-      delay(10);
-      if (count == 50)//35 mph 20 ft long car takes .4 sec to get past given point
-      {
-        break; //Break out and take data if spike becomes long plateau( a tsunami)
-      }
-    }
-    count = 0;
-    PWrange = PWrange + (pulseIn(pwPin, HIGH) / 58); //58us/cm
-    Irange = Irange + (analogRead(analogIn) / (2 * .49)) * (331.33 / IVsound);
-    delay(100);
+  int centigrade = (Ifahrenheit - 32) * 4 / 9;
+  Irange = analogRead(analogIn) * (331.3 / IVsound); //Temperature correction applied, 1 count/cmdistance
+  PWrange = ( pulseIn(pwPin, HIGH) / 58) * (333.1 / IVsound) ; //58us/cm, temperature correction applied
+  I[x]=Irange;
+   PW[x]=PWrange;
   }
-  digitalWrite(pingPermit, LOW); //Hold high to range
-  PWrange = PWrange / 10; //take 10 readings and average
-  Irange = Irange / 10;
+  
+    if(I[0]>I[1] && I[1]> I[2]||I[0]<I[1] && I[1] < I[2])
+    {
+      Irange=I[1];
+    }
+    else
+    {
+     if(I[0]>I[1] && I[2]>I[1] || I[0]<I[1] && I[2]<I[1])
+    {
+      Irange=I[2] ;
+    }
+      else
+      {
+        Irange = I[3];
+      }}
   Serial.print(Irange);
   Serial.println(" CM  Analog output");
   Serial.print(PWrange);
   Serial.println(" CM pulse width output");
+  Serial.print(Ilevel);
+  Serial.println(" CM level");
   Ilevel = Izero - Irange; //  Irange Ilevel &  IzeroIzero global no return statement
 }
 
 void smsMode()
 {
+  int x;
+  char empty[65] = "";
   Serial1.print("AT+CMGF=1\r");    //Because we want to send the SMS in text mode
   delay(500);
+  if (Serial1.available())
+  {
+  for(x=0; x = Serial1.available();  x++)
+  {
+    empty[x] = Serial1.read();
+  }   
+  }
+  Serial.print("Response to sms mode =>");
+  Serial.println(empty);
 }
 
 void SendTextMessage()
@@ -180,14 +238,13 @@ void SendTextMessage()
       Serial.print("AT+CMGS =");
       Serial1.println(phone1);
       Serial.println(phone1);
-      if (!Serial1.available()
-    {
-      Serial.print("waiting on modem to respond to at+cmgs=...");
+      if (!Serial1.available())
+      {
+        Serial.print("waiting on modem to respond to at+cmgs=...");
       }
       while (!Serial1.available())//wait till modem responds with cr
-    {
-      delay(1000);
-
+      {
+        delay(1000);
       }
       //Im just going to assume the cr is what is in the buffer
     }
